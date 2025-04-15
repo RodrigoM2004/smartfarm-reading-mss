6df26a433f1b9071436008d9f248aca142e7c8c9
EnricoSantarelli <enricosantarelli@hotmail.com>
2025-04-15 15:52:27 -0300

    fix: retira obrigatoriedade do role no usuário

--- backend/user_mss/src/models/user_model.js
+++ backend/user_mss/src/models/user_model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user', required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
   address: { type: String, required: true },
   dateOfJoining: { type: Number, required: true }
 });
