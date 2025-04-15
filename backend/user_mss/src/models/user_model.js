e648530b099d4adc7f4908c53124007e30624f4d
Enrico Santarelli <Enrico.Santarelli@ibm.com>
2025-04-15 12:39:06 -0300

    feat: insere mais dados aos modelos de usuário

--- backend/user_mss/src/models/user_model.js
+++ backend/user_mss/src/models/user_model.js
@@ -1,6 +1,6 @@
 const users = [
-  { id: '1', name: 'João Silva', email: 'joao@email.com', password: '123456', role: 'admin' },
-  { id: '2', name: 'Maria Souza', email: 'maria@email.com', password: '123456', role: 'user' }
+  { id: '1', name: 'João Silva', email: 'joao@email.com', password: '123456', role: 'admin', address: "Rua 2, 1000, 3920109", dateOfJoining: 21321983219 },
+  { id: '2', name: 'Maria Souza', email: 'maria@email.com', password: '123456', role: 'user', address: "Rua 1, 2, 093821", dateOfJoining: 20130921903 }
 ];
 
 export default users;
