6c1b5c7ebb595240f4581ab518e7804a3c2209d6
Breno Amorim <brenoamorim11@gmail.com>
2025-05-12 12:34:41 -0300

    feat: map working

--- frontend/src/pages/dashboard/mapPage.jsx
+++ frontend/src/pages/dashboard/mapPage.jsx
@@ -1,3 +1,18 @@
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
+
 export default function MapPage() {
-    return <div className="w-full h-full flex items-center justify-center text-black text-5xl">MapPage</div>
+
+    const position = [51.505, -0.09]
+
+    return <div className="w-full h-full flex items-center justify-center text-black text-5xl">
+        <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      style = {{height: "100px"}}
+    />
+    
+  </MapContainer>
+    </div>
 }
