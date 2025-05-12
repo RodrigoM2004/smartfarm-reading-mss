c3fc3926ba33fa7230433bab01026988aefff114
Breno Amorim <brenoamorim11@gmail.com>
2025-05-12 16:04:58 -0300

    dashboard sidebar start design

--- frontend/src/pages/dashboard/DashboardPage.jsx
+++ frontend/src/pages/dashboard/DashboardPage.jsx
@@ -1,17 +1,22 @@
 import { Outlet } from "react-router-dom"
+import {useState} from "react"
+import { FaThermometerHalf, FaSun, FaFlask, FaBatteryFull, FaMap } from "react-icons/fa"
 
 export default function DashboardPage() {
+
+    const [selectedIndex, setSelectedIndex] = useState(0)
+
     return (
     <div className="w-screen h-screen relative">
-        <div className="w-1/7 h-full bg-blue-800 absolute left-0">
+        <div className="w-1/7 h-full bg-white absolute left-0">
             <div className="flex flex-col w-full h-screen">
                 <div className="w-full h-1/3 "></div>
                 <div className="w-full h-1/3 flex flex-col items-end justify-center gap-2">
-                    <SideBarButton title={"MAPA"}/>
-                    <SideBarButton title={"LUMINOSIDADE"}/>
-                    <SideBarButton title={"pH"}/>
-                    <SideBarButton title={"TEMPERATURA"}/>
-                    <SideBarButton title={"BATERIA"}/>
+                    <SideBarButton title={"MAPA"} myIndex={0} selectedIndex={selectedIndex} icon={<FaMap size={24}/>}/>
+                    <SideBarButton title={"LUMINOSIDADE"} myIndex={1} selectedIndex={selectedIndex} icon={<FaSun size={24}/>}/>
+                    <SideBarButton title={"pH"} myIndex={2} selectedIndex={selectedIndex} icon={<FaFlask size={24}/>}/>
+                    <SideBarButton title={"TEMPERATURA"} myIndex={3} selectedIndex={selectedIndex} icon={<FaThermometerHalf size={24}/>}/>
+                    <SideBarButton title={"BATERIA"} myIndex={4} selectedIndex={selectedIndex} icon={<FaBatteryFull size={24}/>}/>
                 </div>
                 <div className="w-full h-1/3 "></div>   
             </div>
@@ -24,11 +29,30 @@ export default function DashboardPage() {
            )
 }
 
-export function SideBarButton({title}) {
-    return (<div className="w-[90%] h-1/6 flex flex-row">
-        <div className="w-[20%] h-full bg-blue-500"></div>
-        <div className="w-[80%] h-full flex items-center justify-start pl-2">
-            <h1 className="text-white">{title}</h1>
+export function SideBarButton({title, myIndex, selectedIndex, icon, onClick}) {
+
+
+    if(myIndex == selectedIndex){
+        return (
+        <div className={`w-full h-1/6 flex flex-row `}>
+            <div className="w-2 bg-blue-950 rounded-r-sm mr-2"></div>
+            <div className="w-[20%] h-full  flex justify-center items-center text-blue-950">
+            {icon}
+            </div>
+            <div className={`w-[80%] h-full flex items-center justify-start pl-2`}>
+                <h1 className="text-blue-950 font-bold">{title}</h1>
+            </div>
         </div>
-    </div>)
+        )
+    }
+    return (
+    <div className={`w-[90%] h-1/6 flex flex-row  p-1 cursor-pointer`}>
+        <div className="w-[20%] h-full  flex justify-center items-center text-gray-400">
+            {icon}
+            </div>
+        <div className={`w-[80%] h-full flex items-center justify-start pl-2 `}>
+            <h1 className="text-gray-400 font-bold">{title}</h1>
+        </div>
+    </div>
+    )
 }
--- frontend/src/pages/dashboard/mapPage.jsx
+++ frontend/src/pages/dashboard/mapPage.jsx
@@ -1,9 +1,16 @@
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
 
 export default function MapPage() {
 
-    const position = [51.505, -0.09]
+    const position = [-23.6785, -46.7039]
+
+    // const customIcon = L.icon({
+    //     iconUrl: 'your-icon.png', // Path to your custom icon image
+    //     iconSize: [32, 32], // Size of the icon
+    //     iconAnchor: [16, 32], // Point of the icon which will correspond to marker's location
+    //     popupAnchor: [0, -32], // Point from which the popup should open relative to the iconAnchor
+    //   });
 
     return <div className="w-full h-full flex items-center justify-center text-black text-5xl">
         <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
@@ -12,6 +19,11 @@ export default function MapPage() {
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       style = {{height: "100px"}}
     />
+    <Marker position={[-23.6785, -46.7039]} riseOnHover = {true}>
+    <Popup >
+        teste
+    </Popup>
+  </Marker>
     
   </MapContainer>
     </div>
