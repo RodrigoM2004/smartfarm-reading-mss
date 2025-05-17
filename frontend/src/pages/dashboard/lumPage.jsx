62b0270b64c0f1610db87c422c360428feb69151
Breno Amorim <brenoamorim11@gmail.com>
2025-05-17 10:03:15 -0300

    feat:initial dash

--- frontend/src/pages/dashboard/lumPage.jsx
+++ frontend/src/pages/dashboard/lumPage.jsx
@@ -6,7 +6,7 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 export default function LumPage() {
 
         const { setSelectedIndex } = useSidebar()
-        const { userData } = useUser()
+        const { userData, dashboardFilterInitialData, dashboardFilterFinalData } = useUser()
     
         useEffect(() => {
         setSelectedIndex(1)
@@ -16,7 +16,13 @@ export default function LumPage() {
 
     return <div className="w-full h-full flex p-8 text-black text-5xl">
       <div className='w-full h-60'>
-        <div className='w-full h-40 border-2'></div>
+        <div className='w-full h-15  bg-white rounded-md mb-8 p-2'>
+          <div className='w-1/5 h-full bg-blue-950 rounded-md flex items-center justify-center'>
+            <div className='text-white text-sm w-full h-full flex items-center p-2'>
+             {dashboardFilterFinalData}
+            </div>
+          </div>
+        </div>
         <ResponsiveContainer width="100%" height="100%">
           <LineChart data={userData.sensors[1].readings} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
             <CartesianGrid strokeDasharray="3 3" />
@@ -26,15 +32,10 @@ export default function LumPage() {
     fontSize: '14px',
     padding: '5px 10px'
   }}/>
-           <Legend wrapperStyle={{
-    fontSize: '20px',
-    paddingTop: '20px'
-  }}/>
-            <Line type="monotone" dataKey="luminosity" stroke="#8884d8" activeDot={{ r: 8 }} />
+           
+            <Line type="monotone" dataKey="luminosity" stroke="#162456" activeDot={{ r: 8 }} />
           </LineChart>
         </ResponsiveContainer>
       </div>
     </div>
 }
-
-// userData.sensors[1].readings[0].ph
