5a11a8e4e64f05c66fd721a087821d75c71db45a
Breno Amorim <brenoamorim11@gmail.com>
2025-05-20 14:41:21 -0300

    fix: finetuning customList

--- frontend/src/pages/dashboard/components/list/customList.jsx
+++ frontend/src/pages/dashboard/components/list/customList.jsx
@@ -9,11 +9,30 @@ export default function CustomList({ info }) {
     const { userData, dashboardFilterInitialData, dashboardFilterFinalData } = useUser()
     return (
         <>
-        <div className='w-full h-1/9 bg-blue-950 rounded-t-md flex items-center justify-center'></div>
+        <div className='w-full h-1/9 bg-blue-950 rounded-t-md flex items-center justify-between px-2 text-white '>
+          <div className="font-bold w-[19%] text-center">
+            Data
+          </div>
+          <div className="w-[0.5px] h-full bg-gray-400 rounded-full mr-1"></div>
+          <div className="font-bold w-1/5 text-center">
+            Sensor
+          </div>
+          <div className="w-[0.5px] h-full bg-gray-400 rounded-full mr-1"></div>
+          <div className="font-bold  w-1/3 text-center">
+            Leitura de {(info === "lum" ?  "Luminosidade": info === "temp" ? "Tempratura" : info === "batery" ? "Bateria" : info === "ph" ? "pH" : "")}
+          </div>
+          <div className="w-[0.5px] h-full bg-gray-400 rounded-full mr-1"></div>
+          <div className="font-bold text-sm  w-1/5 text-center">
+            % Dif Média
+          </div>
+          
+          
+        </div>
         <div className='w-full h-8/9 rounded-b-md flex overflow-y-scroll flex-col items-center gap-1 px-2 '>
 
              {processListData(userData.sensors, info).map((item, index) => {
                 const localDate = new Date(item.date);
+                localDate.setMinutes(localDate.getMinutes() + localDate.getTimezoneOffset());
                 return (
                   <div key={index} className='w-full flex items-center justify-between p-1 flex-row'>
                     <div className='flex items-center'>
@@ -24,12 +43,12 @@ export default function CustomList({ info }) {
                             year: 'numeric',
                           })}
                         </div>
-                          <div className='mr-2 w-[40%] '>
+                          <div className='mr-2 w-[40%] text-center ml-6'>
                             Sensor #{item.sensorId}
                           </div>
                     </div>
-                    <div className="text-end  w-[40%] h-8 flex items-center justify-end ">
-                      {item.value}%
+                    <div className=" w-[40%] h-8 flex items-center justify-center font-bold text-md ">
+                      {item.value} {info === "lum" || info === "batery" ? "%" : info === "temp" ? "°C" : ""}
                     </div>
                     <div
                       className={`p-2 rounded-md w-18 h-8 flex items-center gap-1 justify-center
