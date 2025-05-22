1b6b6167afcb24482734eed51ac66884afa9c291
Enrico Santarelli <Enrico.Santarelli@ibm.com>
2025-05-22 15:33:50 -0300

    feat: adiciona evento de criação de sensor

--- backend/reading_mss/src/routes/event_routes.js
+++ backend/reading_mss/src/routes/event_routes.js
@@ -2,14 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-const functions = {
-  UserValidate: async () => {
-    await axios.post('http://localhost:3003/eventos', {
-      type: 'UserValidate'
-    })
-  }
-}
-
 router.post('/', async (req, res) => {
   const event = req.body
   await functions[event.type](event.data)
--- backend/user_mss/src/routes/event_routes.js
+++ backend/user_mss/src/routes/event_routes.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import { updateUserByUserId } from '../services/user_service';
 
 const router = express.Router();
+
+const functions = {
+  SensorCreate: async (data) => {
+    await updateUserByUserId(data.userId, data.sensor)
+  },
+}
 
 router.post('/', (req, res) => {
   const event = req.body
